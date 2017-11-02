b864495539f7ee26cf4c764425d17f54130727e7
Cristóbal Silva <crsilva@ing.uchile.cl>
2017-11-02 14:00:48 -0300

    Remove empty method descriptions

--- gpwink_tf/integral.py
+++ gpwink_tf/integral.py
@@ -19,8 +19,9 @@ def integrate_window(left_window, right_window=None, scale_left=1,
     """Solve integral for window-window combination.
 
     Evaluates the integral of same-window multiplication with respect to one
-    variable: ∫ W(tau) W(tau).T dtau
-    :return:
+    variable: 
+
+    ∫ W(tau) W(tau).T dtau
     """
     if right_window is None:
         right_window = left_window
@@ -45,6 +46,7 @@ def integrate_kernel(kernel, scale=(1, 1), shift=(0, 0)):
     """Solve integral for kernel single argument.
 
     Evaluates the integral of a kernel with respect to one variable:
+
     ∫ K(tau, tau') dtau
     """
     kernel = kernel.scale_and_shift(scale, shift)
@@ -64,17 +66,9 @@ def integrate_kernel_window(kernel, window, scale_kernel, shift_kernel,
     integrated with respect to s; output is window in t:
 
     ∫ K(tau, tau') W(tau) dtau
-
-    :param kernel:
-    :param window:
-    :param scale_kernel:
-    :param shift_kernel:
-    :param scale_window:
-    :param shift_window:
-    :param conjugate_right:
-    :return:
     """
     window = window.scale_and_shift(scale_window, shift_window)
+    
     if conjugate_right:
         window = window.conjugate()
     win_sigma, win_s2, win_s, win_1 = window.get_all_params()
@@ -94,6 +88,7 @@ def integrate_kernel_window(kernel, window, scale_kernel, shift_kernel,
     new_ct2 = ct2 + cts**2 / (4 * cs2)
     new_ct = ct + cts * cs / (2 * cs2)
     new_c1 = cs**2 / (4 * cs2) + c1
+    
     return GaussianSquareExponentialWindow(
         sigma=tf.cast(tf.cast(sigma, tf.float64), GLOBAL_DTYPE),
         ct2=tf.cast(tf.cast(new_ct2, tf.float64), GLOBAL_DTYPE),
@@ -116,6 +111,7 @@ def integrate_window_kernel_window(left_window, kernel, right_window,
     left_sigma, left_t2, left_t, left_1 = left_window.get_all_params()
 
     right_window = right_window.scale_and_shift(scale_right, shift_right)
+    
     if conjugate_right:
         right_window = right_window.conjugate()
     right_sigma, right_s2, right_s, right_1 = right_window.get_all_params()
